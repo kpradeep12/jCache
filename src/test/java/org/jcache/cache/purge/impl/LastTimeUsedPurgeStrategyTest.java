e3bda37af72c0408655f16bb42fdd2fb2e19c838
amarjdev <amarjdev@gmail.com>
2018-03-21 10:45:22 -0400

    updated testcase as junit5

--- src/test/java/org/jcache/cache/purge/impl/LastTimeUsedPurgeStrategyTest.java
+++ src/test/java/org/jcache/cache/purge/impl/LastTimeUsedPurgeStrategyTest.java
@@ -1,7 +1,10 @@
 package org.jcache.cache.purge.impl;
 
-import junit.framework.TestCase;
-
-public class LastTimeUsedPurgeStrategyTest extends TestCase {
-
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+public class LastTimeUsedPurgeStrategyTest{
+	@Test
+	void isExpired() {
+		 assertNotNull(null);
+	}
 }
--- src/test/java/org/jcache/cache/purge/impl/TimePeriodExperiedPurgeStrategyTest.java
+++ src/test/java/org/jcache/cache/purge/impl/TimePeriodExperiedPurgeStrategyTest.java
@@ -1,7 +1,10 @@
 package org.jcache.cache.purge.impl;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
 
-import junit.framework.TestCase;
-
-public class TimePeriodExperiedPurgeStrategyTest extends TestCase {
-
+public class TimePeriodExperiedPurgeStrategyTest {
+	@Test
+	void isExpired() {
+		 assertNotNull(null);
+	}
 }
